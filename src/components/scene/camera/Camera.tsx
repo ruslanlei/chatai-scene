a6163d83720cc4073d3332eeb37cb60579dbf478
Ruslan Musaev <rusya.lay@gmail.com>
2023-09-27 19:41:59 +0400

    -

--- src/components/scene/camera/Camera.tsx
+++ src/components/scene/camera/Camera.tsx
@@ -1,20 +1,37 @@
-import {FC, useCallback, useEffect} from "react";
+import {FC, useCallback, useEffect, useRef} from "react";
 import {useThree} from "react-three-fiber";
 import {OrbitControls, PerspectiveCamera} from "@react-three/drei";
 import { Vector3 } from 'three';
+import { useSpring } from "@react-spring/three";
 
 const defaultPosition: [number, number, number] = [0, 0, 2];
 
 export const Camera: FC = () => {
   const { camera } = useThree();
+  const cameraRef = useRef(camera);
+  useEffect(() => {
+    cameraRef.current = camera;
+  }, [camera]);
 
+  const [, set] = useSpring(() => ({
+    position: defaultPosition,
+    config: {
+      mass: 1,
+      tension: 170,
+      friction: 26,
+    },
+    onChange: ({ value: { position } } ) => {
+      console.log(...position);
+      cameraRef.current.lookAt(new Vector3(0, 0, 0));
+      cameraRef.current.position.set(...position);
+    }
+  }));
   useEffect(() => {
     camera.lookAt(new Vector3(0, 0, 0));
   }, [camera]);
 
   const onCameraEnd = useCallback(() => {
-    camera.lookAt(new Vector3(0, 0, 0));
-    camera.position.set(...defaultPosition);
+    set({ position: defaultPosition, from: { position: camera.position.toArray() } });
   }, [camera]);
 
   return (
