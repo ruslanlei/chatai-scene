9a86389a042232f9704d106ca14c135307e51e59
Ruslan Musaev <rusya.lay@gmail.com>
2023-09-27 19:32:30 +0400

    -

--- src/components/scene/camera/Camera.tsx
+++ src/components/scene/camera/Camera.tsx
@@ -1,13 +1,20 @@
-import {FC, useEffect} from "react";
+import {FC, useCallback, useEffect} from "react";
 import {useThree} from "react-three-fiber";
 import {OrbitControls, PerspectiveCamera} from "@react-three/drei";
 import { Vector3 } from 'three';
+
+const defaultPosition: [number, number, number] = [0, 0, 2];
 
 export const Camera: FC = () => {
   const { camera } = useThree();
 
   useEffect(() => {
     camera.lookAt(new Vector3(0, 0, 0));
+  }, [camera]);
+
+  const onCameraEnd = useCallback(() => {
+    camera.lookAt(new Vector3(0, 0, 0));
+    camera.position.set(...defaultPosition);
   }, [camera]);
 
   return (
@@ -19,11 +26,12 @@ export const Camera: FC = () => {
         maxPolarAngle={1.4}
         minAzimuthAngle={-1}
         maxAzimuthAngle={1}
+        onEnd={onCameraEnd}
       />
       <PerspectiveCamera
         makeDefault
         fov={40}
-        position={[0, 0, 2]}
+        position={defaultPosition}
       />
     </>
   )
